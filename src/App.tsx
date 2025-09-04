0e5aa76b2abdbb22173b4bef02773036c13de3a8
shashanka2a <37143174+shashanka2a@users.noreply.github.com>
2025-09-04 08:18:02 -0400

    update packages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Hero } from "./components/Hero";
 import { Features } from "./components/Features";
 import { CallToAction } from "./components/CallToAction";
 import { Footer } from "./components/Footer";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export default function App() {
   return (
@@ -14,6 +16,8 @@ export default function App() {
         <CallToAction />
       </main>
       <Footer />
+      <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
