dd8dd7870d0a008c20a8452b7bae9132c306e6c1
shashanka2a <37143174+shashanka2a@users.noreply.github.com>
2025-09-04 07:40:03 -0400

    update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
 import { Features } from '@/components/Features'
-import { Testimonials } from '@/components/Testimonials'
 import { CallToAction } from '@/components/CallToAction'
 import { Footer } from '@/components/Footer'
 
@@ -18,7 +17,6 @@ export default function Home() {
         <main>
           <Hero />
           <Features />
-          <Testimonials />
           <CallToAction />
         </main>
         <Footer />
