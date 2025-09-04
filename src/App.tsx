56d7f085ced67cea67c183b55bdee785c8e71af5
shashanka2a <37143174+shashanka2a@users.noreply.github.com>
2025-09-04 07:49:12 -0400

    remove  testimonilas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import { Features } from "./components/Features";
-import { Testimonials } from "./components/Testimonials";
 import { CallToAction } from "./components/CallToAction";
 import { Footer } from "./components/Footer";
 
@@ -12,7 +11,6 @@ export default function App() {
       <main>
         <Hero />
         <Features />
-        <Testimonials />
         <CallToAction />
       </main>
       <Footer />
